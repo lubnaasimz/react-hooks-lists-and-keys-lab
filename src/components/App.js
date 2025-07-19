b11cf5630bd4623565e3ffc1d3a765d24e196fdb
lubnaa <lubnaamuhammed@gmail.com>
2025-07-19 04:30:03 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,15 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+import projectData from "../data/projects"; 
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home />
+      <About />
+      <ProjectList projects={projectData} />
     </div>
   );
 }
---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    id: 1,
+    name: "Project One",
+    about: "This is my first project.",
+    technologies: ["React", "CSS", "JavaScript"]
+  },
+  {
+    id: 2,
+    name: "Project Two",
+    about: "This is my second project.",
+    technologies: ["Node.js", "Express"]
+  },
+  {
+    id: 3,
+    name: "Project Three",
+    about: "This is my third project.",
+    technologies: ["MongoDB", "Mongoose"]
+  }
+];
+
+export default projects;
